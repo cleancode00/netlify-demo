e46de36111e8029e72b7321dbef590300354b97d
cleancode00 <83853864+cleancode00@users.noreply.github.com>
2021-09-23 21:54:37 +0800

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+module.exports = { 
+    entry: ['./index.js', './app.jsx'],
+    output: { 
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './'),
+        publicPath: '/',
+    },
+    module: {
+        //rules的值是一個陣列可以存放多個loader物件
+        rules: [
+            { test: /.jsx$/, exclude: /node_modules/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-react','@babel/preset-env'] } } },
+            { test: /.js$/, exclude: /node_modules/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-env'] } } }
+        ]
+    },
+     //增加一個給devserver的設定
+     devServer: {   
+        static: './',
+        historyApiFallback:true,
+         //指定開啟port為9000   
+        port: 9000  
+    } 
+};
